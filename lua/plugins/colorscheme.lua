c5a76837027254709042deecd83a30aa2ca09376
Paul Gessinger <hello@paulgessinger.com>
2024-06-08 12:41:45 +0200

    switch to onedark-light

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,25 @@
+-- return {
+--   -- add gruvbox
+--   { "ellisonleao/gruvbox.nvim" },
+--
+--   -- Configure LazyVim to load gruvbox
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "gruvbox",
+--     },
+--   },
+-- }
+
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  { "navarasu/onedark.nvim" },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "onedark",
     },
   },
 }
